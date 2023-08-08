14aea59f43060b216fb3fe5d2cc43d1d5a61912f
mujtabasameer <139006645+mujtabasameer@users.noreply.github.com>
2023-08-09 03:17:46 +0500

    Add files via upload

---
+++ 4.to-do list/todolist.js
@@ -0,0 +1,49 @@
+import inquirer from 'inquirer';
+const todolist = [];
+async function add() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'todo',
+            message: 'Add to your list:',
+        }
+    ]);
+    todolist.push(answers.todo);
+}
+function display() {
+    let count = 1;
+    console.log("Here are your todo-list items:");
+    for (let i = 0; i < todolist.length; i++) {
+        console.log(`${count}. ${todolist[i]}`);
+        count++;
+    }
+}
+async function main() {
+    let check = true;
+    while (check === true) {
+        const choice = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'Welcome to todo-list app.',
+                choices: ['Add', 'Display', 'Exit'],
+            }
+        ]);
+        let choice2 = choice.choice;
+        switch (choice2) {
+            case 'Add':
+                await add();
+                break;
+            case 'Display':
+                display();
+                break;
+            case 'Exit':
+                console.log("Thank you for using this app!.");
+                check = false;
+                break;
+            default:
+                console.log("Invalid choice!");
+        }
+    }
+}
+main();
